0948ffb9136d083cff54cc7b204137bdee60341f
Robert Laye <2000rob@live.co.uk>
2023-12-11 20:42:13 +0000

    Initialised backend
    I have set up the node server and installed the necessary dependencies. I've also connected the server to a MongoDB database.

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@hottakes.akpjuev.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log("Connect to database.")
+    }).catch((error) => {
+        console.log('Can\'t connect to database.');
+        console.log(error);
+    })
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use((req, res) => {
+    res.json({ message: 'Your request was successful!' });
+});
+
+module.exports = app;
